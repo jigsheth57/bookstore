1529dcd30ab1e2aa804155038b0cefc3ca213946
jigsheth57 <jigsheth@yahoo.com>
2022-02-14 10:36:53 -0600

    update

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,12 +1,15 @@
 package com.example.bookstore.controller;
 
+import com.example.bookstore.entities.Book;
 import com.example.bookstore.repos.BookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,4 +30,12 @@ public class BookController {
         model.addAttribute("book", bookRepository.findByTitle(title));
         return "book";
     }
+
+    @PostMapping("/book-search")
+    public String searchBook(@ModelAttribute("book") Book book) {
+        System.out.println(book.getTitle());
+        // model.addAttribute("book", bookRepository.findByTitle(title));
+        return "book-search";
+    }
+
 }
