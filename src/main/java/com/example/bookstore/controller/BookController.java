f9a513a639300d805e134b30e0b23bee3181942a
jigsheth57 <jigsheth@yahoo.com>
2022-02-19 08:42:05 -0600

    search 2

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +26,21 @@ public class BookController {
 
     private static final Log log = LogFactory.getLog(BookController.class);
 
-    @GetMapping
+    @GetMapping("/")
     public String homePage(Model model) {
+        model.addAttribute("book", new Book());
         model.addAttribute("books", bookRepository.findAll());
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String homePage(@ModelAttribute Book book, Model model) {
+        log.info("title=" + book.getTitle());
+        if (book.getTitle() != null || !book.getTitle().isEmpty()) {
+            model.addAttribute("books", bookRepository.findByTitleContaining(book.getTitle()));
+        } else {
+            model.addAttribute("books", bookRepository.findAll());
+        }
         return "index";
     }
 
@@ -35,17 +48,6 @@ public class BookController {
     public String getBook(Model model, @PathVariable String title) {
         model.addAttribute("book", bookRepository.findByTitle(title));
         return "book";
-    }
-
-    @PostMapping("/book-search")
-    public String searchBook(@RequestParam(value = "title", required = false) String title, Model model) {
-        log.info("title=" + title);
-        if (title != null || !title.isEmpty()) {
-            model.addAttribute("books", bookRepository.findByTitleContaining(title));
-        } else {
-            model.addAttribute("books", bookRepository.findAll());
-        }
-        return "index";
     }
 
 }
--- src/main/java/com/example/bookstore/entities/Book.java
+++ src/main/java/com/example/bookstore/entities/Book.java
@@ -25,7 +25,7 @@ public class Book {
     @Column(nullable = true)
     private String description;
 
-    protected Book() {
+    public Book() {
         super();
     }
 
