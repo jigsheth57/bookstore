945a5a3631baf8f4a36b09d78cab76d4f3633c10
jigsheth57 <jigsheth@yahoo.com>
2022-02-19 08:06:46 -0600

    search

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,16 +1,20 @@
 package com.example.bookstore.controller;
 
-import com.example.bookstore.entities.Book;
 import com.example.bookstore.repos.BookRepository;
+import com.example.bookstore.entities.Book;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -18,6 +22,8 @@ public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
+
+    private static final Log log = LogFactory.getLog(BookController.class);
 
     @GetMapping
     public String homePage(Model model) {
@@ -32,10 +38,14 @@ public class BookController {
     }
 
     @PostMapping("/book-search")
-    public String searchBook(@ModelAttribute("book") Book book) {
-        System.out.println(book.getTitle());
-        // model.addAttribute("book", bookRepository.findByTitle(title));
-        return "book-search";
+    public String searchBook(@RequestParam(value = "title", required = false) String title, Model model) {
+        log.info("title=" + title);
+        if (title != null || !title.isEmpty()) {
+            model.addAttribute("books", bookRepository.findByTitleContaining(title));
+        } else {
+            model.addAttribute("books", bookRepository.findAll());
+        }
+        return "index";
     }
 
 }
--- src/main/java/com/example/bookstore/repos/AuthorRepository.java
+++ src/main/java/com/example/bookstore/repos/AuthorRepository.java
@@ -1,8 +1,11 @@
 package com.example.bookstore.repos;
 
+import java.util.List;
+
 import com.example.bookstore.entities.Author;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface AuthorRepository extends PagingAndSortingRepository<Author, Long> {
+    List<Author> findByNameContaining(String name);
 }
